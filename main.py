806e0fc226960978a673ada1eba530d494cc068c
harrylzh <89496934+harrylzh@users.noreply.github.com>
2022-07-20 14:55:09 +0800

    更新 main.blocks, main.py

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-def 数字显示定时(数字: number):
-    basic.show_number(数字)
-    basic.clear_screen()
+@namespace
+class CodeLib:
+    # % blockId=CodeLib_PrintNumber block="数字显示定时|数字 %value1"
+    # % color="#006400"    
+    # % weight=97
+    # % blockGap=10
+    # % value1.min=0 value1.max=255 
+    # % name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    def PrintNumber(value1: number):
+        basic.show_number(value1)
+        basic.clear_screen()
+    # % blockId=CodeLib_HelloWorld block="HelloWorld"
+    # % color="#006400"
+    # % weight=87
+    # % blockGap=10    
+    # % name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    def HelloWorld():
+        basic.show_string("Hello World!")
+        basic.clear_screen()
