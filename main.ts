d83545aaca40f9aea88081399e55bb2686e8d787
harrylzh <89496934+harrylzh@users.noreply.github.com>
2022-07-20 14:32:45 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,11 @@
-function 数字显示定时 (数字: number) {
+namespace CodeLib{
+//% blockId=CodeLib_PrintNumber block="PrintNumber"
+    //% color="#006400"
+    //% weight=87
+    //% blockGap=10
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+export function 数字显示定时 (数字: number) {
     basic.showNumber(数字)
     basic.clearScreen()
+}
 }
